b730a184d6f629ab6cc7a81c4f63e68b508c7211
KarpenkoOS <81673841+KarpenkoOS@users.noreply.github.com>
2021-11-09 20:31:07 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingleTon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sinlett sin = Sinlett.VizovSingl();
+            Console.WriteLine($"Счётчик при вызрве инстанса {Sinlett.Zahod}");
+
+            Sinlett sin_2 = Sinlett.VizovSingl();
+            Console.WriteLine($"Счётчик при вызрве инстанса {Sinlett.Zahod}");
+
+            Console.ReadLine();
+
+        }
+    }
+
+    
+    public class Sinlett
+    {
+        public static int Zahod { get; set; }
+        private static object Zahod0 = null;
+        public static Sinlett sn;
+        private Sinlett()
+        {
+            ++Zahod;
+        }
+
+        public static Sinlett VizovSingl()
+        {
+            if (sn == Zahod0)
+            {
+                sn = new Sinlett();
+            }
+
+            return sn;
+        }
+    }
+}
